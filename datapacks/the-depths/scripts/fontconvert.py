fe192ac5b5103b318d7503ca8d074fcf0c5b9151
Possseidon <xpossseidon@gmail.com>
2022-03-09 19:22:48 +0100

    Add a small font conversion utility.

---
+++ datapacks/the-depths/scripts/fontconvert.py
@@ -0,0 +1,16 @@
+import sys
+
+
+small_capitals = "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘqʀꜱᴛᴜᴠᴡxyᴢ"
+
+
+def convert(text: str, font: str) -> str:
+    return "".join(map(lambda char: font[code - ord('A')] if ord('A') <= (code := ord(char.upper())) <= ord('Z') else char, text))
+
+
+def main():
+    print(convert(" ".join(sys.argv[1:]), small_capitals))
+
+
+if __name__ == "__main__":
+    main()
